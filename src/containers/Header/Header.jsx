f7fd8044a0dc471400c762433517370e188b9db8
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-05-17 19:52:34 +0200

    header updated

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -16,8 +16,27 @@ const scaleVariants = {
 };
 const Header = () => {
   return (
-    <div className="app__header app__flex">
-      <motion.div
+    <motion.div
+      whileInView={{ y: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header"
+    >
+      <p className="p-text">
+        <span>👋</span>
+        Hi My name is
+      </p>
+      <h2 className="name-text">Abdulhamid Afolabi</h2>
+
+      <h2 className="name-text">
+        <span>
+          I love coding for the web <br /> &lt;&gt;one line at a time&lt;/&gt;
+        </span>
+      </h2>
+      <a href="#work" className="header__link">
+        {" "}
+        Check out my Work
+      </a>
+      {/* <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
@@ -60,8 +79,8 @@ const Header = () => {
             <img src={circle} alt="circle" />
           </div>
         ))}
-      </motion.div>
-    </div>
+      </motion.div> */}
+    </motion.div>
   );
 };
 
