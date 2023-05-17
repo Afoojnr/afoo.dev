bcfac9457180ae38548a0263ae392a7f327e0ae3
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-05-17 21:41:19 +0200

    about updated

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -30,28 +30,48 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that <span>Good Apps</span> <br /> means{" "}
-        <span>Good Business</span>
+        <span> About</span> me
       </h2>
-      <div className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="app__profile-item"
-            key={about.title + index}
+      {/* <div className="app__profiles"> */}
+      <motion.div
+        whileInView={{ opacity: 1 }}
+        // whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.5, type: "tween" }}
+        className="app__profile-item"
+        // key={about.title + index}
+      >
+        <img src={images.about02} alt="Afolabi" />
+        <p className="p-text">
+          <span style={{ color: "black" }}> 👋 Hello! I'm Abdulhamid </span>{" "}
+          <br />
+          Despite pursuing my Master's degree in Material Science, my unwavering
+          passion for web development remains at the forefront. While my
+          academic focus lies in Material Science, I am dedicated to expanding
+          my knowledge and skills in the dynamic realm of web development. By
+          day
+          <span
+            style={{
+              color: "gold",
+            }}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
-        ))}
-      </div>
+            ☼
+          </span>{" "}
+          , I delve into the world of material properties prediction using
+          machine learning, while by night{" "}
+          <span
+            style={{
+              color: "blue",
+            }}
+          >
+            ☾
+          </span>
+          , I channel my creativity as a front-end web developer. I'm passionate
+          about leveraging both disciplines to create stunning websites and
+          unlock insights in the field of materials science. Let's bring
+          innovation to life together!
+        </p>
+      </motion.div>
+      {/* </div> */}
     </>
   );
 };
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { easeInOut, motion } from "framer-motion";
-import { images } from "../../constants";
+import { motion } from "framer-motion";
+
 import "./Header.scss";
 import { AppWrap } from "../../wrapper";
 
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
 const Header = () => {
   return (
     <motion.div
@@ -29,57 +19,12 @@ const Header = () => {
 
       <h2 className="name-text">
         <span>
-          I love coding for the web <br /> &lt;&gt;one line at a time&lt;/&gt;
+          I love coding for the web <br /> &lt;&gt; one line at a time &lt;/&gt;
         </span>
       </h2>
       <a href="#work" className="header__link">
-        {" "}
         Check out my Work
       </a>
-      {/* <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Afolabi</h1>
-            </div>
-          </div>
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer</p>
-            <p className="p-text">Freelancer</p>
-          </div>
-        </div>
-      </motion.div>
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="overlay_circle"
-          src={images.circle}
-          alt="profile_circle"
-        ></motion.img>
-      </motion.div>
-      <motion.div
-        variants={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="circle" />
-          </div>
-        ))}
-      </motion.div> */}
     </motion.div>
   );
 };
